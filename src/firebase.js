58a9b06a81194e9ad3e834a62cf2a05d48e3468b
Matija <morsulic11@gmail.com>
2021-01-04 20:54:10 +0100

    sportko#part5_init

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+var firebaseConfig = {
+    apiKey: "AIzaSyB84YfuxVlX8N_XFJ82QuQ28sjU-UyFK-Q",
+    authDomain: "sportko-daf1d.firebaseapp.com",
+    projectId: "sportko-daf1d",
+    storageBucket: "sportko-daf1d.appspot.com",
+    messagingSenderId: "140017467426",
+    appId: "1:140017467426:web:e7a55f2e9e9a37b61c8a7b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export  {
+      firebase,
+  };
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -10,6 +10,7 @@
               <label for="exampleInputEmail1">Email address</label>
               <input
                 type="email"
+                v-model="username"
                 class="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
@@ -23,21 +24,23 @@
               <label for="exampleInputPassword1">Password</label>
               <input
                 type="password"
+                v-model="password"
                 class="form-control"
                 id="exampleInputPassword1"
-                placeholder="Password"
+                placeholder="Password" 
               />
             </div>
             <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
+              <label for="exampleInputPassword2">Repeat Password</label>
               <input
                 type="password"
+                v-model="passwordRepeat"
                 class="form-control"
-                id="exampleInputPassword1"
+                id="exampleInputPassword2"
                 placeholder="Password"
               />
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+           <button type="button" @click="signup" class="btn btn-primary">Submit</button>
           </form>
         </div>
         <div class="col-sm"></div>
@@ -46,5 +49,31 @@
   </div>
 </template>
 <script>
-export default {};
+import { firebase } from "@/firebase.js";
+export default {
+    name:'Signup',
+    data() {
+        return {
+            username: '',
+            password: '',
+            passwordRepeat: '',
+        
+        };
+    },
+
+    methods: {
+        signup() {
+            firebase.auth().createUserWithEmailAndPassword(this.username, this.password).then(
+             function(){
+                 console.log("Uspješna registracija");
+             })
+            .catch(function(error){
+              console.error("Došlo je do pogreške",error)
+            });
+
+
+            console.log("Nastavak");
+        },
+    }, 
+};
 </script>
